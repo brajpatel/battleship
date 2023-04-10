4429adeac44d870b7c62a63dbf66acbf31c51bbc
brajpatel <brajeshpatel0002@gmail.com>
2023-04-10 21:14:06 +0100

    Add test for ship name property on gameboard elements

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,9 +1,21 @@
 import { Gameboard } from "../components/gameboard";
 
 describe("gameboard", () => {
-    test("gameboard is an array of length 100", () => {
+    test("gameboard is an array of length 10", () => {
         const gameboard = new Gameboard();
 
         expect(gameboard.board.length).toBe(10);
     });
+
+    test("elements in the gameboard array are objects", () => {
+        const gameboard = new Gameboard();
+
+        expect(gameboard.board[0][3] instanceof Object).toBe(true);
+    })
+
+    test("objects in the gameboard array contain ship name property", () => {
+        const gameboard = new Gameboard();
+
+        expect(gameboard.board[6][1].hasOwnProperty('shipName')).toBe(true);
+    })
 })
