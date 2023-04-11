a0e33c81f38a6200b3c1da13b111941da14ae1a5
brajpatel <brajeshpatel0002@gmail.com>
2023-04-11 20:28:20 +0100

    Import player class to index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import { Player } from "./components/player";
 import { Ship } from "./components/ship";
 import { Gameboard } from "./components/gameboard";
+
+const playerShips = document.getElementById('player-ships');
+const htmlCarrier = document.getElementById('carrier');
+const htmlBattleship = document.getElementById('battleship');
+const htmlCruiser = document.getElementById('cruiser');
+const htmlSubmarine = document.getElementById('submarine');
+const htmlDestroyer = document.getElementById('destroyer');
 
 // player ships
 const carrier = new Ship(5);
@@ -14,3 +22,4 @@ const aiBattleship = new Ship(4);
 const aiCruiser = new Ship(3);
 const aiSubmarine = new Ship(3);
 const aiDestroyer = new Ship(2);
+
