ccb5a22e1c6dd8c3af887a879e94f9b45005a4a9
brajpatel <brajeshpatel0002@gmail.com>
2023-04-11 09:59:41 +0100

    Add tests for invalid placement of ships on squares occupied by other ships

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -10,41 +10,62 @@ describe("gameboard", () => {
 
     test("the board array contains subarrays of length 10", () => {
         expect(gameboard.board[8].length).toBe(10);
-    })
+    });
 
     test("elements in the gameboard subarrays are objects", () => {
         expect(gameboard.board[0][3] instanceof Object).toBe(true);
-    })
+    });
 
     test("objects in the gameboard array contain ship name property", () => {
         expect(gameboard.board[6][1].hasOwnProperty('shipName')).toBe(true);
-    })
+    });
 
     test("objects in the gameboard array contain a ship index property", () => {
         expect(gameboard.board[7][7].hasOwnProperty('shipIndex')).toBe(true);
-    })
+    });
 
     test("a ship placed within the board's parameters returns true 1", () => {
         const submarine = new Ship(3);
 
         expect(gameboard.checkValidShipPlacement(submarine.getShipLength(), 4, 6)).toBe(true);
-    })
+    });
 
     test("a ship placed within the board's parameters returns true 2", () => {
         const carrier = new Ship(5);
 
         expect(gameboard.checkValidShipPlacement(carrier.getShipLength(), 9, 0)).toBe(true);
-    })
+    });
 
     test("a ship cannot be placed outside the board's parameters and returns false 1", () => {
         const battleship = new Ship(4);
 
         expect(gameboard.checkValidShipPlacement(battleship.getShipLength(), 1, 8)).toBe(false);
-    })
+    });
 
-    test("a ship cannot be placed outside the board's parameters and returns false 1", () => {
+    test("a ship cannot be placed outside the board's parameters and returns false 2", () => {
         const destroyer = new Ship(2);
 
         expect(gameboard.checkValidShipPlacement(destroyer.getShipLength(), 7, 9)).toBe(false);
+    });
+
+    test("a ship with valid placement will be placed on to the board", () => {
+        const submarine = new Ship(3);
+        const gameboard = new Gameboard();
+        gameboard.placeShip(submarine, 5, 5);
+
+        expect(gameboard.board[5][5].shipName).toBe(submarine);
     })
-})
+
+    test("", () => {
+        
+    })
+
+    test("a ship cannot be placed on board squares occupied by other ships", () => {
+        const carrier = new Ship(5);
+        const battleship = new Ship(4);
+        const gameboard = new Gameboard();
+        gameboard.placeShip(carrier, 7, 4);
+
+        expect(gameboard.placeShip(battleship, 7, 1)).toBe(false);
+    })
+});
