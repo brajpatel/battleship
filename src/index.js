bdb42361db35f481a40dc7605132a15c864c130f
brajpatel <brajeshpatel0002@gmail.com>
2023-04-12 11:18:33 +0100

    Create and style player board

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ const htmlCruiser = document.getElementById('cruiser');
 const htmlSubmarine = document.getElementById('submarine');
 const htmlDestroyer = document.getElementById('destroyer');
 
+// create player boards
+const playerBoard = new Gameboard();
+
+// create players
+const player = new Player('');
+
 // player ships
 const carrier = new Ship(5);
 const battleship = new Ship(4);
@@ -23,3 +29,41 @@ const aiCruiser = new Ship(3);
 const aiSubmarine = new Ship(3);
 const aiDestroyer = new Ship(2);
 
+// create boards
+createBoard('player-board');
+
+function createBoard(boardName) {
+    const board = document.getElementById(`${boardName}`);
+
+    for(let i = 0; i < 10; i++) {
+        for(let j = 0; j < 10; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.setAttribute('data-x', j);
+            cell.setAttribute('data-y', i);
+
+            if(boardName === 'ai-board') {
+                cell.addEventListener('click', (e) => {
+                    attackEnemy(e.target);
+                });
+            }
+            else if(boardName === 'player-board') {
+                cell.addEventListener('dragover', (e) => e.preventDefault());
+                cell.addEventListener('drop', (e) => {
+                    e.preventDefault();
+                    dropShip(e);
+                });
+            }
+
+            board.appendChild(cell);
+        }
+    }
+}
+
+function dropShip(e) {
+
+}
+
+function attackEnemy(target) {
+
+}
